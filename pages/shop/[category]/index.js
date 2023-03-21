bf56967a58482e291afc0e6e0e9a1dc7b3764431
vishal9643 <amitkrmishra864@gmail.com>
2023-03-21 22:35:57 +0530

    hover change

--- pages/shop/[category]/index.js
+++ pages/shop/[category]/index.js
@@ -122,48 +122,58 @@ const CategoryPage = ({category, products}) => {
 export default CategoryPage;
 
 const ProductItem = ({ product }) => {
-  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <CustomLink
       href={`/shop/${useRouter().query.category}/${product.slug}`}
       className="grid gap-1"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
     >
-      <div className="relative">
-        {isHovered && (
-          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-10" />
-        )}
+      {/* <div className="relative">
         {product.salePercentage > 0 && (
-          <div className="absolute bg-red-500 py-1 px-2 text-white text-sm z-20">
+          <div className="absolute bg-red-500 py-1 px-2 text-white text-sm">
             -{product.salePercentage}%
           </div>
         )}
         <img src={product.images?.[0]?.url} alt={product.name} />
-      </div>
-      <h3 className="text-lg sm:text-xl font-semibold uppercase">
+      </div> */}
+      {/* <h3 className="text-lg sm:text-xl font-semibold uppercase">
         {product.name}
       </h3>
       {product.reviews?.length > 0 && (
         <div className="flex flex-col sm:flex-row  gap-1">
           <div className="flex flex items-center gap-1">
-            {[1, 2, 3, 4, 5].map((star) => (
+            {[1, 2, 3, 4, 5].map((i) => (
               <FaStar
-                key={star}
-                className={`${
-                  star <= product.ratingsAverage ? "text-yellow-500" : ""
-                }`}
+                key={i}
+                className={i > i ? "text-gray-400" : "text-black"}
               />
             ))}
-            <p>({product.reviews.length} reviews)</p>
           </div>
-          <p className="text-gray-500 text-sm">
-            {product.ratingsAverage.toFixed(1)} average rating
-          </p>
+          <div>| 10 Reviews</div>
         </div>
       )}
-      <p className="text-gray-500">${product.sizes[0].price.toFixed(2)}</p>
+      <p className="">${product.sizes[0].price}</p> */}
+       <div className="product-item mt-2 py-2">
+        <img
+          src={product.images?.[0]?.url}
+          alt={product.name}
+          className="rounded-xl"
+          //width={350}
+          //height={350}
+          style={{ 
+            height: 300, 
+            width: "100%", 
+            '&:hover img': { 
+              transform: 'scale(1.2)' 
+            }
+          }}
+        />
+        <h3 className="text-lg sm:text-xl font-semibold mt-2 font-montserrat text-white text-center">
+        {product.name[0].toUpperCase()}{product.name.slice(1).toLowerCase()}
+		
+        </h3>
+        <p className="text-white font-montserrat mb-2 text-center mt-1">₹{product.sizes[0].price}</p>
+      </div>
     </CustomLink>
   );
 };
